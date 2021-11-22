5cf1ac8b724510712ab6dba67a984726917b360e
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-21 22:41:40 -0500

    Creado la Clase Rectangulo con logica similar a la de cuadrado

--- Cuadrado.py
+++ Cuadrado.py
@@ -9,3 +9,6 @@ class Cuadrado(FiguraGeometrica, Color):
 
     def calcular_area(self):
         return self.ancho * self.alto
+
+    def __str__(self):
+        return f'Cuadrado: {FiguraGeometrica.__str__(self)},{Color.__str__(self)}'
